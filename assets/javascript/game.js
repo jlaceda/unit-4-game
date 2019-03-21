b2992cd6ba8328f1bc4191e9f53ba5c244500384
John Laceda <john.laceda@gmail.com>
2019-03-21 01:32:42 -0700

    update characters and add image to fighter mapping

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,8 @@ let Game =
 {
 	LukeSkywalker: {},
 	ObiwanKenobi: {},
-	EmperorPalpatine: {},
-	DarthMaul: {},
+	Rey: {},
+	KyloRen: {},
 	
 	allFighters: [],
 	playerCharacter: {},
@@ -15,25 +15,34 @@ let Game =
 
 	gameOver: false,
 
+	nameImageMap: {
+		"Luke Skywalker": "assets/images/luke.png",
+		"Obi-wan Kenobi": "assets/images/kenobi.png",
+		"Rey": "assets/images/rey.png",
+		"Kylo Ren": "assets/images/kylo.jpg",
+	},
+
 	initGame: function()
 	{
 		this.gameOver = false;
 		// this is where all Fighters are created.
-		this.LukeSkywalker = new Fighter("Luke Skywalker", 110, 10, 8);
+		this.LukeSkywalker = new Fighter("Luke Skywalker", 140, 4, 30);
 		this.ObiwanKenobi = new Fighter("Obi-wan Kenobi", 120, 8, 11);
-		this.EmperorPalpatine = new Fighter("Emperor Palpatine", 140, 4, 30);
-		this.DarthMaul = new Fighter("Darth Maul", 130, 9, 25);
+		this.Rey = new Fighter("Rey", 110, 10, 8);
+		this.KyloRen = new Fighter("Kylo Ren", 130, 9, 25);
 
 		this.allFighters = [
 			this.LukeSkywalker,
 			this.ObiwanKenobi,
-			this.EmperorPalpatine,
-			this.DarthMaul
+			this.Rey,
+			this.KyloRen
 		];
 
 		this.playerCharacter = {};
 		this.currentDefender = {};
 		this.enemiesOnDeck = [];
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+		this.allFighters.map(f => f.image = this.nameImageMap[f.name]);
 	},
 	// choose a fighter by name
 	// enemiesOnDeck and playerCharacter should be filled after this
